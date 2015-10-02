9250851839efa3d4d62a120581a7f4212e2a91c3
Phuong 'J' Le H <le.hoai.phuong@framgia.com>
2015-10-02 14:45:59 +0700

    Init AWSElasticSearch

---
+++ lib/aws/elasticsearch.rb
@@ -0,0 +1,7 @@
+require "aws/elasticsearch/version"
+
+module Aws
+  module Elasticsearch
+    # Your code goes here...
+  end
+end
---
+++ lib/aws/elasticsearch/version.rb
@@ -0,0 +1,5 @@
+module Aws
+  module Elasticsearch
+    VERSION = "0.0.1"
+  end
+end
